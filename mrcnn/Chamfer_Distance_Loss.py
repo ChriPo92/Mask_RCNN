Fixed wrong shape of the Rotation Loss
This lead to an error during validation of the epoch, probably
because Keras can't handle multi dimensional losses.

mrcnn/Chamfer_Distance_Loss.py
@@ -246,15 +246,15 @@ def mrcnn_pose_loss_graph_keras(target_poses, target_class_ids, pred_trans, pred
     # to get an orthoganl matrix from any 3x3 matrix we use an SVD where
     # a = U * diag(S) * V^h
     # TODO: There seem to be Nans in the Gradient of the SVD
-    # s, u, v = SVD(name="mrcnn_pose_loss/pred_rot_svd")(y_pred_r)
-    # y_pred_r = KL.Lambda(lambda y: tf.linalg.matmul(y[0], y[1]),
-    #                      name="mrcnn_pose_loss/pred_rot_svd_matmul")([u, v])
+    s, u, v = SVD(name="mrcnn_pose_loss/pred_rot_svd")(y_pred_r)
+    y_pred_svd_r = KL.Lambda(lambda y: tf.linalg.matmul(y[0], y[1]),
+                          name="mrcnn_pose_loss/pred_rot_svd_matmul")([u, v])
     pos_xyz_models = KL.Lambda(lambda y: tf.gather(y[0], y[1]),
                                name="mrcnn_pose_loss/pos_xyz_models",
                                output_shape=lambda s:  (s[1][0], s[0][1], s[0][2]))(
                                [xyz_models, positive_class_ids])  # [pos_ix, 3, N]
 
-    chamfer_loss = chamfer_distance_loss_keras(y_pred_r, y_pred_t, y_true_r, y_true_t, pos_xyz_models)
+    chamfer_loss = chamfer_distance_loss_keras(y_pred_svd_r, y_pred_t, y_true_r, y_true_t, pos_xyz_models)
     # rot_loss = KL.Lambda(lambda y: tf.reduce_mean(tf.keras.losses.mae(y[0], y[1])),
     #                         name="mrcnn_pose_loss/rot_error")([y_true_r, y_pred_r])
     # rot_loss = KL.Lambda(lambda y: tf.sqrt(tf.reduce_mean(tf.keras.losses.mse(y[0], y[1])) + 1e-8),
@@ -262,7 +262,13 @@ def mrcnn_pose_loss_graph_keras(target_poses, target_class_ids, pred_trans, pred
     # [pos_ix]
     rot_trace = KL.Lambda(lambda y: tf.linalg.trace(tf.matmul(y[0], tf.linalg.inv(y[1]))),
                           name="mrcnn_pose_loss/rot_trace")([y_pred_r, y_true_r])
-    rot_loss = KL.Lambda(lambda y: tf.math.acos(tf.clip_by_value(tf.multiply(y - tf.ones_like(y), 0.5), -1, 1)), name="mrcnn_pose_loss/rot_error")(rot_trace)
+    rot_loss = KL.Lambda(lambda y: tf.reduce_mean(
+        tf.math.acos(
+            tf.clip_by_value(
+                tf.multiply(y - tf.ones_like(y), 0.5),
+                -0.999999, 0.999999)
+        )
+    ), name="mrcnn_pose_loss/rot_error")(rot_trace)
     trans_loss = KL.Lambda(lambda y: tf.sqrt(tf.reduce_mean(tf.keras.losses.mse(y[0], y[1])) + 1e-8),
                          name="mrcnn_pose_loss/trans_error")([y_true_t, y_pred_t])
     huber_trans_loss = KL.Lambda(lambda y: tf.reduce_mean(huber_loss(tf.norm(y[0]-y[1], axis=-1), 2.0)),

mrcnn/pointnet_pose_estimation.py
@@ -456,19 +456,19 @@ def build_fpn_pointnet_pose_graph(rois, feature_maps, depth_image, image_meta, m
                                        name="centered_concat_point_clouds")([pcl_list, feature_list])
         # [batch, num_rois, num_classes, 6]
         rot = build_PointNet_Keras_Graph(concat_point_cloud, num_points, config,
-                                         train_bn, "rot", 7, 6,
+                                         train_bn, "rot", 7, 9,
                                          last_activation="tanh",
-                                         vector_size=int(1.5*config.POINTNET_VECTOR_SIZE))
+                                         vector_size=int(1.0*config.POINTNET_VECTOR_SIZE))
 
     # [batch, num_rois, 3, 1, num_classes]
     trans = KL.Lambda(lambda y: tf.transpose(tf.squeeze(y, axis=3), [0, 1, 3, 4, 2]), name="trans_reshape")(trans)
     # [batch, num_rois, 3, 2, num_classes]
     rot = KL.Reshape((config.TRAIN_ROIS_PER_IMAGE, config.NUM_CLASSES,
-                     3, 2), name="rot_reshape")(rot)
+                     3, 3), name="rot_reshape")(rot)
     rot = KL.Lambda(lambda y: tf.transpose(y, [0, 1, 3, 4, 2]))(rot)
     # [batch, num_rois, 3, 3, num_classes]; uses orthogonality of rotation matrices to calc
     # the third column vector
-    rot = CalcRotMatrix(name="CalcRotMatrix", config=config)(rot)
+    #rot = CalcRotMatrix(name="CalcRotMatrix", config=config)(rot)
 
     # print_op = tf.print([tf.shape(feature_list), tf.shape(pcl_list), tf.shape(point_cloud_repr),
     #                      tf.shape(x), tf.shape(shared), rot, trans])