eed26d987aea039e6f0e3972fbfa7219c08b7270
Christoph Pohl <christoph.pohl@kit.edu>
2019-03-22 10:46:33 +0100

    Class Residual Dense Layers for the FC3 Layer in Pose Estimation
    The Residuals are calculated for each class differently for Rot
    and Trans. The Trans error goes down now the same way it did with
    just a single Dense layer, but the Rotation does not learn as well
    as it did with complete class T-Nets.

--- mrcnn/pointnet_pose_estimation.py
+++ mrcnn/pointnet_pose_estimation.py
@@ -216,12 +216,28 @@ def build_PointNet_Keras_Graph(point_cloud_tensor, num_points, config, train_bn,
     x = KL.BatchNormalization(
         name=f'mrcnn_pointnet_{name}_bn7')(x, training=train_bn)
     x = KL.Activation('relu')(x)
+    x_res = KL.Lambda(lambda y: tf.reshape(y, (config.BATCH_SIZE, config.TRAIN_ROIS_PER_IMAGE,
+                                          config.NUM_CLASSES * 128)))(x)
+    unit_multiplicator = config.NUM_CLASSES
     # [batch, num_rois, num_classes, out_number]
-    x = Dense2D(config.NUM_CLASSES, unit_multiplicator * out_number,
-                 name=f"mrcnn_pointnet_{name}_fc3")(x)
+    x_res = KL.Dense(unit_multiplicator * out_number,
+                 name=f"mrcnn_pointnet_{name}_fc3_res")(x_res)
+    x_res = KL.BatchNormalization(
+        name=f'mrcnn_pointnet_{name}_bn8')(x_res, training=train_bn)
+    x_res = KL.Activation('relu')(x_res)
+    x_res = KL.Dense(unit_multiplicator * out_number,
+                     name=f"mrcnn_pointnet_{name}_fc3_res2")(x_res)
+    x_res = KL.BatchNormalization(
+        name=f'mrcnn_pointnet_{name}_bn9')(x_res, training=train_bn)
     # if name is "rot":
-    #     x = KL.Lambda(lambda y: tf.reshape(y, (config.BATCH_SIZE, config.TRAIN_ROIS_PER_IMAGE,
-    #                                            config.NUM_CLASSES, out_number)))(x)
+    x_res = KL.Lambda(lambda y: tf.reshape(y, (config.BATCH_SIZE, config.TRAIN_ROIS_PER_IMAGE,
+                                           config.NUM_CLASSES, out_number)))(x_res)
+    x_res = KL.BatchNormalization(
+        name=f'mrcnn_pointnet_{name}_bn10')(x_res, training=train_bn)
+    x = KL.Dense(out_number, name=f"mrcnn_pointnet_{name}_fc3")(x)
+    x = KL.BatchNormalization(
+        name=f'mrcnn_pointnet_{name}_bn11')(x, training=train_bn)
+    x = KL.Add()([x, x_res])
     return x
 
 class CalcRotMatrix(KL.Layer):
